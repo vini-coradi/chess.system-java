e81f0b3c25f88f800add5b44a254c65cb3b2c531
vini-coradi <vinicesar.coradi@gmail.com>
2020-04-28 14:45:28 -0300

    ChessException and ChessPosition

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -1,5 +1,14 @@
 package boardgame;
 
-public class BoardException {
+public class BoardException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+		
+	}
+	
+	
 
 }
